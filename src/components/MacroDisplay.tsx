9cfdf84d21d29b67cd583ea7fdbc120e93028328
LSanchez17 <galaxyplayeremail@gmail.com>
2024-07-15 00:33:27 -0400

    ability to edit macros and save/reload from local storage

--- src/components/MacroDisplay.tsx
+++ src/components/MacroDisplay.tsx
@@ -2,24 +2,14 @@ import { Macro } from "./Macro"
 import { Macros } from "../hooks/useGetLocalStorage"
 import useGetLocalStorage from "../hooks/useGetLocalStorage"
 
-import { Button } from "@mui/base"
-
-
 export const MacroDisplay = () => {
-    const dinahMacros: Macros = useGetLocalStorage('dinah')
-    const luisMaros: Macros = useGetLocalStorage('luis')
-
-    const handleClick = () => {
-        console.log('clicked')
-    }
+    const dinahMacros: Macros | null = useGetLocalStorage('Dinah')
+    const luisMacros: Macros | null = useGetLocalStorage('Luis')
 
     return (
         <div className='macroDetails'>
-            <Macro {...dinahMacros} name={'Dinah'} />
-            <Button 
-                onClick={handleClick} 
-            >Save Macros</Button>
-            <Macro {...luisMaros} name={'Luis'}/>
+            {dinahMacros && <Macro {...dinahMacros} name={'Dinah'} />}
+            {luisMacros && <Macro {...luisMacros} name={'Luis'}/>}
         </div>
     )
 
--- src/hooks/useGetLocalStorage.ts
+++ src/hooks/useGetLocalStorage.ts
@@ -6,17 +6,13 @@ export interface Macros {
     fats: number;
 }
 
-const EMPTY_MACROS = {
-    protein: 0,
-    carbs: 0,
-    fats: 0,
-}
 
 const useGetLocalStorage = (key: string) => {
-    const [data, setData] = useState<Macros>(EMPTY_MACROS);
+    const [data, setData] = useState<Macros | null>(null);
+    const keyToSearch = `${key}Macros`;
 
     useEffect(() => {
-        const storedData = localStorage.getItem(`${key}Macros`);
+        const storedData = localStorage.getItem(keyToSearch);
         if (storedData) {
             setData(JSON.parse(storedData));
         }
